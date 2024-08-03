518a777dd065bb187428293eafabaa2d586204e2
H3rm1tR3b0rn <davigalbuquerque@gmail.com>
2024-08-03 00:00:14 -0300

    Main file corrected

--- wed.py
+++ wed.py
@@ -4,21 +4,18 @@ sys.path.append(".")
 import mod.userpass as userpass
 import mod.addends as addends
 import mod.fuzzy as fuzzy
+import mod.mix as mix
 import getopt
 
 def help(argv):
-    print("Usage: "+argv[0]+" <mode> <options>")
-    print("Modes allowed:")
-    print("help           -- Print this help page")
-    print("userpass       -- Create a file with user pass in each line separeted by space or other separators")
-    print("fuzzy          -- fuzzy wordlists with 1337 or other symbols")
-    print("addends        -- input wordlist to add 123, !@#, etc at the end of each word")
+    print(" Usage: "+argv[0]+" <mode> <options>")
+    print(" Modes allowed:")
+    print("     help           -- Print this help page")
+    print("     userpass       -- Create a file with user pass in each line separeted by space or other separators")
+    print("     fuzzy          -- fuzzy wordlists with 1337 or other symbols")
+    print("     addends        -- input wordlist to add 123, !@#, etc at the end of each word")
+    print("     mix            -- mix 2 files of possible passwords")
     print("")
-    userpass.help(0)
-    print()
-    addends.help(0)
-    print()
-    fuzzy.help(0)
 
 
 def main():
@@ -27,7 +24,7 @@ def main():
         exit()
     else:
         mode = str(sys.argv[1])
-    modes = ["help", "userpass", "fuzzy", "addends"]
+    modes = ["help", "userpass", "fuzzy", "addends", "mix"]
     if mode not in modes:
         help(sys.argv)
     elif mode == "help":
@@ -44,5 +41,9 @@ def main():
         code = fuzzy.main(sys.argv)
         if code != 0:
             fuzzy.help(code);
+    elif mode == "mix":
+        code = mix.main(sys.argv)
+        if code != 0:
+            mix.help(code);
 
 main()
